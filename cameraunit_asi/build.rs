13ed5579f7312a93156b3a957091c5515d83a1c5
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2023-09-14 01:31:20 -0400

    Build

--- cameraunit_asi/build.rs
+++ cameraunit_asi/build.rs
@@ -73,7 +73,7 @@ fn main() {
         .write_to_file(out_path)
         .expect("Couldn't write bindings!");
 
-    cc::Build::new()
+    // cc::Build::new()
     //     .file("src/lib.c")
-        .compile("cameraunit_asi");
+    //     .compile("cameraunit_asi");
 }
